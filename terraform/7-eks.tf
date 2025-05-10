39b44952d6dc0033c043994e0011e81cd1937fd1
SV <cloud@svestor.link>
2025-05-10 23:08:22 +0300

    eks-tpl: add vpc/+/subnets/+/igw/+/ngw/+/routes/+/eks

---
+++ terraform/7-eks.tf
@@ -0,0 +1,43 @@
+resource "aws_iam_role" "eks" {
+    name = "${local.eks_cluster_name}-${local.env}-iam-role"
+
+    assume_role_policy = jsonencode({
+        Version = "2012-10-17"
+        Statement = [
+            {
+                Action = "sts:AssumeRole"
+                Effect = "Allow"
+                Principal = {
+                    Service = "eks.amazonaws.com"
+                }
+            }
+        ]
+    })
+
+    tags = local.common_tags
+}
+
+resource "aws_iam_role_policy_attachment" "eks" {
+    role = aws_iam_role.eks.name
+    policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+resource "aws_eks_cluster" "eks" {
+    name = "${local.eks_cluster_name}-${local.env}"
+    role_arn = aws_iam_role.eks.arn
+    version = local.eks_cluster_version
+
+    vpc_config {
+        subnet_ids = [for subnet in aws_subnet.private : subnet.id]
+        endpoint_public_access = true
+        endpoint_private_access = false
+    }
+
+    access_config {
+        authentication_mode = "API"
+        bootstrap_cluster_creator_admin_permissions = true 
+    }
+
+    depends_on = [aws_iam_role_policy_attachment.eks]
+}
+
