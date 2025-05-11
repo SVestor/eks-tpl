8077d5fd37faa80bb3fb3dc7ea247d8d15dcd5da
SV <cloud@svestor.link>
2025-05-11 19:03:31 +0300

    eks-tpl: add vpc/+/subnets/+/igw/+/ngw/+/routes/+/eks_cp/+/workers_ng

--- terraform/7-eks.tf
+++ terraform/7-eks.tf
@@ -1,43 +1,43 @@
 resource "aws_iam_role" "eks" {
-    name = "${local.eks_cluster_name}-${local.env}-iam-role"
+  name = "${local.eks_cluster_name}-${local.env}-cluster-control-plane-create-iam-role"
 
-    assume_role_policy = jsonencode({
-        Version = "2012-10-17"
-        Statement = [
-            {
-                Action = "sts:AssumeRole"
-                Effect = "Allow"
-                Principal = {
-                    Service = "eks.amazonaws.com"
-                }
-            }
-        ]
-    })
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+      }
+    ]
+  })
 
-    tags = local.common_tags
+  tags = local.common_tags
 }
 
-resource "aws_iam_role_policy_attachment" "eks" {
-    role = aws_iam_role.eks.name
-    policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
+  role       = aws_iam_role.eks.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
 }
 
 resource "aws_eks_cluster" "eks" {
-    name = "${local.eks_cluster_name}-${local.env}"
-    role_arn = aws_iam_role.eks.arn
-    version = local.eks_cluster_version
+  name     = "${local.eks_cluster_name}-${local.env}"
+  role_arn = aws_iam_role.eks.arn
+  version  = local.eks_cluster_version
 
-    vpc_config {
-        subnet_ids = [for subnet in aws_subnet.private : subnet.id]
-        endpoint_public_access = true
-        endpoint_private_access = false
-    }
+  vpc_config {
+    subnet_ids              = [for subnet in aws_subnet.private : subnet.id]
+    endpoint_public_access  = true
+    endpoint_private_access = false
+  }
 
-    access_config {
-        authentication_mode = "API"
-        bootstrap_cluster_creator_admin_permissions = true 
-    }
+  access_config {
+    authentication_mode                         = "API"
+    bootstrap_cluster_creator_admin_permissions = true
+  }
 
-    depends_on = [aws_iam_role_policy_attachment.eks]
+  depends_on = [aws_iam_role_policy_attachment.eks_cluster_policy]
 }
 
