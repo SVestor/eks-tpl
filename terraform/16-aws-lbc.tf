c533e05e59169b08365a12283077d0cde3c77c7c
SV <cloud@svestor.link>
2025-05-19 23:38:37 +0300

    eks-tpl: add aws-load-balancer-controller

---
+++ terraform/16-aws-lbc.tf
@@ -0,0 +1,69 @@
+data "aws_iam_policy_document" "aws_lbc" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["pods.eks.amazonaws.com"]
+    }
+
+    actions = [
+      "sts:AssumeRole",
+      "sts:TagSession"
+    ]
+  }
+}
+
+resource "aws_iam_role" "aws_lbc" {
+  name = "${local.eks_cluster_name}-${local.env}-aws-lbc-iam-role"
+
+  assume_role_policy = data.aws_iam_policy_document.aws_lbc.json
+
+  tags = local.common_tags
+}
+
+resource "aws_iam_policy" "aws_lbc" {
+  name = "AmazonEKSLoadBalancerControllerPolicy"
+
+  policy = file("./iam/AmazonEKSLoadBalancerControllerPolicy.json")
+}
+
+resource "aws_iam_role_policy_attachment" "aws_lbc" {
+  role       = aws_iam_role.aws_lbc.name
+  policy_arn = aws_iam_policy.aws_lbc.arn
+}
+
+resource "aws_eks_pod_identity_association" "aws_lbc" {
+  cluster_name    = aws_eks_cluster.eks.name
+  namespace       = "kube-system"
+  service_account = "aws-load-balancer-controller"
+  role_arn        = aws_iam_role.aws_lbc.arn
+}
+
+resource "helm_release" "aws_lbc" {
+  name = "aws-load-balancer-controller"
+
+  repository = "https://aws.github.io/eks-charts"
+  chart      = "aws-load-balancer-controller"
+  namespace  = "kube-system"
+  version    = "1.13.2"
+
+   set {
+    name  = "clusterName"
+    value = aws_eks_cluster.eks.name
+  }
+
+  set {
+    name  = "serviceAccount.name"
+    value = "aws-load-balancer-controller"
+  }
+
+  set {
+    name  = "vpcId"
+    value = aws_vpc.eks_vpc.id
+  }
+
+  depends_on = [aws_eks_node_group.eks_main]
+}
+  
+
