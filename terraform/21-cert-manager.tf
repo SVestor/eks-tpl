c57a42954d189294cf0aaf8c6b16de5001b8fba9
SV <cloud@svestor.link>
2025-05-25 22:21:07 +0300

    eks-tpl: add cert-manager /+/ add sa for DNS01 challenge

--- terraform/21-cert-manager.tf
+++ terraform/21-cert-manager.tf
@@ -12,6 +12,27 @@ resource "helm_release" "cert_manager" {
     value = true
   }
 
+  # (optional) Used for the DNS01 challenge
+
+  set {
+    name  = "serviceAccount.name"
+    value = "cert-manager"
+  }
+
+  # (optional) Update file system permissions
+  # The reason for this optional step is that on EKS Fargate and on some older versions of EKS you may observe errors
+  # related to file permissions. This is a known issue with cert-manager and EKS.
+
+  set {
+    name  = "securityContext.fsGroup"
+    value = 1001
+  }
+
+  set {
+    name  = "securityContext.runAsUser"
+    value = 1001
+  }
+
   depends_on = [helm_release.ingress_nginx_external]
 }
-  
+
