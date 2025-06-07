05fc29077c823c0b822930de0188ceda95bbd9f9
SV <cloud@svestor.link>
2025-06-07 18:14:35 +0300

    eks-tpl: add helm grafana template // to generate grafana template manifests

---
+++ Monitoring/example-1/terraform/13-grafana-render.tf
@@ -0,0 +1,43 @@
+# grafana render template
+
+data "helm_template" "grafana" {
+  name = "grafana"
+
+  repository       = "https://grafana.github.io/helm-charts"
+  chart            = "grafana"
+  namespace        = "monitoring"
+  create_namespace = true
+  version          = "9.2.1"
+
+  set {
+    name  = "image.tag"
+    value = "12.0.0"
+  }
+
+  set {
+    name  = "adminPassword"
+    value = "mySuperSecretPassword"
+  }
+
+  set {
+    name  = "persistence.enabled"
+    value = "true"
+  }
+
+  set {
+    name  = "persistence.storageClassName"
+    value = "gp3-monitoring"
+  }
+
+  set {
+    name  = "persistence.size"
+    value = "8Gi"
+  }
+}
+
+resource "local_file" "grafana_manifests" {
+  for_each = data.helm_template.grafana.manifests
+
+  filename = "target/${each.key}"
+  content  = each.value
+}
